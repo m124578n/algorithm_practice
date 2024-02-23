fa62593538239f608fdb0e812265136a17a307cc
m124578n <m23568n@gmail.com>
2024-02-23 11:49:46 +0800

    add/ find minimum index

--- python/minimum.py
+++ python/minimum.py
@@ -7,8 +7,17 @@ def minimum(target: list) -> int | float:
             ans = x            
     return ans
 
+def find_minimum_index(target: list) -> int | float:
+    ans, idx = target[0], 0
+    for i, v in enumerate(target):
+        if v < ans:
+            ans, idx = v, i
+    return idx
+
 
 if __name__ == '__main__':
     target = [1, 2, 3, 4, 5, 0, 99, 87, 55, -50, -35]
     ans = minimum(target)
+    print(ans)
+    ans = find_minimum_index(target)
     print(ans)
