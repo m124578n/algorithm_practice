33709044fb1478718e3c4298495919990ce8aa09
m124578n <m23568n@gmail.com>
2024-02-23 11:35:28 +0800

    add/ sorting by swap

---
+++ python/sorting_by_swap.py
@@ -0,0 +1,30 @@
+
+
+def swap(a, b):
+    '''in python we can just a, b = b, a'''
+    return b, a
+
+
+def sorting_by_swap(target: list) -> list:
+    n = len(target)
+    is_sorting = False
+    while not is_sorting:
+        for x in range(0, n-1):
+            if target[x] > target[x+1]:
+                target[x], target[x+1] = target[x+1], target[x]
+        is_sorting = check_is_sorting(target)
+    return target
+
+
+def check_is_sorting(target: list) -> bool:
+    n = len(target)
+    for x in range(0, n-1):
+        if target[x] > target[x+1]:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    target = [5, 2, 0, 4, 9, 8, 7, 100, -5]
+    ans = sorting_by_swap(target)
+    print(ans)
